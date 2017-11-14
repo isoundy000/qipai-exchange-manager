4ec67d75b2393da5c4bccbb3c3fdfb2e845d7b90
besthyhy <besthyhy@gmail.com>
2017-11-14 16:59:18 +0800

    分页

--- goods/goods-list.js
+++ goods/goods-list.js
@@ -26,19 +26,14 @@ window.onresize = function () {
     console.log(window.getComputedStyle(table, null).width);
 }
 
-var isFirstPage = 1;
-var isLastPage = 0;
+var isFirstPage = -1;
+var isLastPage = -1;
 var pageIndex = 0;
-var pageSize = 2;
+var pageSize = -1;
 var totalPages = -1;
 var totalRecords = -1;
 
 requestOnePage(pageIndex, 2);
-
-// tbody.removeChild();
-// requestOnePage(0,2);
-// requestOnePage(1,2);
-// requestOnePage(2,2);
 
 
 function requestOnePage(index, size) {
@@ -91,7 +86,7 @@ function requestOnePage(index, size) {
             totalPages = json.pager.totalPages;
             totalRecords = json.pager.totalRecords;
 
-
+            updatePageMarkers();
         }
     }
 }
@@ -118,12 +113,176 @@ function appendTdAndData(tr, data) {
     td.innerHTML = data;
     return td;
 }
+function updatePageMarkers() {
+    document.getElementById("total").innerText = "总共" + totalPages + "页";
 
-document.getElementById("previous").onkeydown = function () {
+    var page0 = document.getElementById("page0");
+    var page1 = document.getElementById("page1");
+    var page2 = document.getElementById("page2");
+    var page3 = document.getElementById("page3");
+    var page4 = document.getElementById("page4");
+    var page5 = document.getElementById("page5");
+    var page6 = document.getElementById("page6");
+    var pageDot1 = document.getElementById("page-dot1");
+    var pageDot2 = document.getElementById("page-dot2");
+
+
+    page1.style.display = "none";
+    page2.style.display = "none";
+    page3.style.display = "none";
+    page4.style.display = "none";
+    page5.style.display = "none";
+
+    page1.style.backgroundColor = "#ffffff";
+    page2.style.backgroundColor = "#ffffff";
+    page3.style.backgroundColor = "#ffffff";
+    page4.style.backgroundColor = "#ffffff";
+    page5.style.backgroundColor = "#ffffff";
+
+
+    if (totalPages <= 5) {
+        switch (totalPages) {
+            case 1:
+                page1.style.display = "flex";
+                page1.innerText = 1;
+                break;
+            case 2:
+                page1.style.display = "flex";
+                page1.innerText = 1;
+                page2.style.display = "flex";
+                page2.innerText = 2;
+                break;
+            case 3:
+                page1.style.display = "flex";
+                page1.innerText = 1;
+                page2.style.display = "flex";
+                page2.innerText = 2;
+                page3.style.display = "flex";
+                page3.innerText = 3;
+                break;
+            case 4:
+                page1.style.display = "flex";
+                page1.innerText = 1;
+                page2.style.display = "flex";
+                page2.innerText = 2;
+                page3.style.display = "flex";
+                page3.innerText = 3;
+                page4.style.display = "flex";
+                page4.innerText = 4;
+
+                break;
+            case 5:
+                page1.style.display = "flex";
+                page1.innerText = 1;
+                page2.style.display = "flex";
+                page2.innerText = 2;
+                page3.style.display = "flex";
+                page3.innerText = 3;
+                page4.style.display = "flex";
+                page4.innerText = 4;
+                page5.style.display = "flex";
+                page5.innerText = 5;
+                break;
+
+        }
+
+
+
+
+        switch (pageIndex) {
+            case 0:
+                page1.style.backgroundColor = "#8CD7DC";
+                break
+            case 1:
+                page2.style.backgroundColor = "#8CD7DC";
+                break
+            case 2:
+                page3.style.backgroundColor = "#8CD7DC";
+                break
+            case 3:
+                page4.style.backgroundColor = "#8CD7DC";
+                break
+            case 4:
+                page5.style.backgroundColor = "#8CD7DC";
+                break
+            case 5:
+
+                break
+        }
+    }
+
+    if (totalPages > 5) {
+
+        page1.style.display = "flex";
+        page2.style.display = "flex";
+        page3.style.display = "flex";
+        page4.style.display = "flex";
+        page5.style.display = "flex";
+
+        var currentPage = pageIndex;
+
+        if (pageIndex <= 2) {
+            page1.innerText = 1;
+            page2.innerText = 2;
+            page3.innerText = 3;
+            page4.innerText = 4;
+            page5.innerText = 5;
+
+            switch (pageIndex){
+                case 0:
+                    page1.style.backgroundColor = "#8CD7DC";
+                    break;
+                case 1:
+                    page2.style.backgroundColor = "#8CD7DC";
+                    break;
+                case 2:
+                    page3.style.backgroundColor = "#8CD7DC";
+                    break
+            }
+        }
+        if (pageIndex >= totalPages - 3) {
+            page1.innerText = totalPages - 4;
+            page2.innerText = totalPages - 3;
+            page3.innerText = totalPages - 2;
+            page4.innerText = totalPages - 1;
+            page5.innerText = totalPages ;
+
+            switch (pageIndex){
+                case totalPages-1:
+                    page5.style.backgroundColor = "#8CD7DC";
+                    break;
+                case totalPages-2:
+                    page4.style.backgroundColor = "#8CD7DC";
+                    break;
+                case totalPages-3:
+                    page3.style.backgroundColor = "#8CD7DC";
+                    break
+            }
+
+
+        }
+
+        if (pageIndex > 2 && pageIndex < totalPages - 3) {
+
+            page1.innerText = pageIndex - 1;
+            page2.innerText = pageIndex;
+            page3.innerText = pageIndex + 1;
+            page4.innerText = pageIndex + 2;
+            page5.innerText = pageIndex + 3;
+
+            page3.style.backgroundColor = "#8CD7DC";
+
+        }
+    }
+
+
+}
+
+document.getElementById("previous").onclick = function () {
 
     if (isFirstPage != 1) {
         clearTable();
-        requestOnePage(pageIndex-1,pageSize);
+        requestOnePage(pageIndex - 1, pageSize);
     }
 
 };
@@ -132,18 +291,55 @@ document.getElementById("next").onclick = function () {
 
     if (isLastPage != 1) {
         clearTable();
-        requestOnePage(pageIndex+1,pageSize);
+        requestOnePage(pageIndex + 1, pageSize);
     }
+
+};
+document.getElementById("first").onclick = function () {
+
+    clearTable();
+    requestOnePage(0, pageSize);
+
+
+};
+document.getElementById("end").onclick = function () {
+    clearTable();
+    requestOnePage(totalPages - 1, pageSize);
+
 
 };
 
 document.getElementById("goto-page").onkeydown = function (e) {
     if (e.which == 13) {
-        alert(this.value);
+        console.log(this.value);
+        console.log(Number(this.value));
+        if (this.value >= 0 && this.value <= totalPages) {
+            clearTable();
+            requestOnePage(this.value, pageSize);
+        }
+
     }
-    // if (this.text != 1) {
-    //     clearTable();
-    //     requestOnePage(pageIndex+1,pageSize);
-    // }
+
 
 };
+
+document.getElementById("page1").onclick = function () {
+    clearTable();
+    requestOnePage(Number(this.innerText) - 1, pageSize);
+}
+document.getElementById("page2").onclick = function () {
+    clearTable();
+    requestOnePage(Number(this.innerText) - 1, pageSize);
+}
+document.getElementById("page3").onclick = function () {
+    clearTable();
+    requestOnePage(Number(this.innerText) - 1, pageSize);
+}
+document.getElementById("page4").onclick = function () {
+    clearTable();
+    requestOnePage(Number(this.innerText) - 1, pageSize);
+}
+document.getElementById("page5").onclick = function () {
+    clearTable();
+    requestOnePage(Number(this.innerText) - 1, pageSize);
+}
